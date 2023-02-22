7940a3f48f2e45300070e034a2e5318faccfaf05
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-02-22 14:57:27 +0500

    userInput

--- DynamicArray/DynamicArray/Program.cs
+++ DynamicArray/DynamicArray/Program.cs
@@ -9,18 +9,20 @@ namespace DynamicArray
             int sizeArray = 1;
             int[] array = new int[sizeArray];
             int[] tempArray = new int[array.Length + 1];
-            string text = "";
+            string userInput = "";
+            string textSum = "sum";
+            string exit = "exit";
             int numbers;
             int sum = 0;
 
-            Console.WriteLine("Введите целое число для сложения введите sum для выхода введите exit\n");
+            Console.WriteLine($"Введите целое число. Введите {textSum} для сложения. Bведите {exit} для выходы из программы \n") ;
 
-            while (text != "exit")
+            while (userInput != exit)
             {
                 Console.WriteLine("eneter number,sum or exit");
 
-                text = Console.ReadLine();
-                int.TryParse(text, out numbers);
+                userInput = Console.ReadLine();
+                int.TryParse(userInput, out numbers);
 
                 for (int i = 0; i < array.Length; i++)
                 {
@@ -35,7 +37,7 @@ namespace DynamicArray
                     sum += array[i];
                 }
 
-                if (text == "sum")
+                if (userInput == textSum)
                 {
                     Console.WriteLine(sum);
                 }
